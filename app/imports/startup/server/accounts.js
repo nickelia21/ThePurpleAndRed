82bc6137d038a2da69b72b272d50b6d4562f254a
Nick Elia <elianicholas@gmail.com>
2019-06-11 17:37:04 -0400

    general fixes

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -20,7 +20,11 @@ function createUser(email, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    Meteor.settings.defaultAccounts.map(({
+      email,
+      password,
+      role
+    }) => createUser(email, password, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
